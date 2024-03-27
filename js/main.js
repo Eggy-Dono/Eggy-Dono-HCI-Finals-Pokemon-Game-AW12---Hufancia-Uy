47677d46fc8f299b19cda290d9d430793876dff2
Tired Egg <153513465+Eggy-Dono@users.noreply.github.com>
2024-03-27 18:50:46 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const game = new Game();
+
+game.drawBlackScreen();
+
+document.querySelector('.start-btn-overlay').addEventListener('click', (ev) => {
+  document.querySelector('.black--screen').remove();
+  document.querySelector('.welcome-introduction').remove();
+  document.querySelector('.pointer-arrow').remove();
+  game.drawIntroScreen();
+  document.querySelector('.start-btn-overlay').remove();
+  document.querySelector('.container').classList.add('big-device');
+  document.querySelector('.power-btn').style.fill = '#26c426';
+
+  setTimeout(() => {
+    game.drawWelcomeScreen();
+  }, 2500);
+});
